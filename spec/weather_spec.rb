f993ae4f6305cebc93ab286f6c1e28e2fd29c728
Tech-A12 <Tech-A12@Tech-A12s-MacBook-Air.local>
2017-08-30 15:08:34 +0100

    Added extra rspec tests

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -24,7 +24,7 @@ describe WeatherApi do
 			expect(@city_name_result).not_to be_empty
 		end
 
-		it "should return a hash of results" do
+		it "should return a hash of results for key - coord" do
 			expect(@city_name_result["coord"]).to be_kind_of(Hash)
 		end
 
@@ -54,7 +54,38 @@ describe WeatherApi do
 			expect(@city_name_result["coord"].keys[1]).to eq("lat")
 		end
 
+		it "should return a hash of results for key - weather" do
+			expect(@city_name_result["weather"]).to be_kind_of(Array)
+		end
 
+		it "the weather array should contain a hash" do
+			expect(@city_name_result["weather"][0]).to be_a(Hash)
+		end
+
+		it "the weather array should contain a key called id with data type string" do
+			expect(@city_name_result["weather"][0]).to include("id")
+			expect(@city_name_result["weather"][0].keys[0].class).to be(String)
+			
+		end
+
+		it "in the weather array the key id should have a value of type integer" do
+			expect(@city_name_result["weather"][0].values[0].class).to eq(Fixnum)
+		end
+
+		it "the weather array should contain a key called main with data type string" do
+			expect(@city_name_result["weather"][0]).to include("main")
+			expect(@city_name_result["weather"][0].keys[1].class).to be(String)
+		end
+
+		it "the weather array should contain a key called description with data type string" do
+			expect(@city_name_result["weather"][0]).to include("description")
+			expect(@city_name_result["weather"][0].keys[2].class).to be(String)
+		end
+
+		it "the weather array should contain a key called icon with data type string" do
+			expect(@city_name_result["weather"][0]).to include("icon")
+			expect(@city_name_result["weather"][0].keys[3].class).to be(String) 
+		end
 
 	end
 
